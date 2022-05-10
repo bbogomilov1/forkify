d4ff4e767719538881851acd8f7d227079721bba
bbogomilov1 <bdbogomilov@gmail.com>
2022-05-10 15:58:45 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '839cdcb2-262e-48da-81cf-579be41c621d';
+export const MODAL_CLOSE_SEC = 2.5;
